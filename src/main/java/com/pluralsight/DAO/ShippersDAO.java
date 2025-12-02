03138bef904171befe3951a3c9e5f32cfda9794b
Sthealth <AW@example.com>
2025-12-02 13:55:18 -0500

    implemented code to shippersDAO, layout for shipper information model, and update pom.xml

--- src/main/java/com/pluralsight/DAO/ShippersDAO.java
+++ src/main/java/com/pluralsight/DAO/ShippersDAO.java
@@ -1,4 +1,90 @@
 package com.pluralsight.DAO;
 
+import com.pluralsight.models.Shipper;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShippersDAO {
+    private DataSource dataSource;
+
+    public ShippersDAO(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void insertNewShipper(String companyName, String companyPhoneNumber) {
+        try {
+            String query = """
+                    INSERT INTO shippers (CompanyName, Phone)
+                    VALUES (?, ?)
+                    """;
+
+            try (Connection connection = dataSource.getConnection();
+
+                 PreparedStatement preparedStatement = connection.prepareStatement(query)
+            ) {
+                preparedStatement.setString(1, companyName);
+                preparedStatement.setString(2, companyPhoneNumber);
+
+                preparedStatement.executeUpdate();
+            }
+        } catch (Exception ex) {
+            System.out.println("Error occurred!");
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public List<Shipper> displayAllShippers() {
+        List<Shipper> allShippers = new ArrayList<>();
+
+        try {
+            String query = """
+                    SELECT ShipperID, CompanyName, Phone
+                    FROM shippers
+                    """;
+            try (Connection connection = dataSource.getConnection();
+
+                 PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+                 ResultSet results = preparedStatement.executeQuery()
+            ) {
+                while (results.next()) {
+                    int shipperID = results.getInt(1);
+                    String companyName = results.getString(2);
+                    String companyPhone = results.getString(3);
+
+                    allShippers.add(new Shipper(shipperID, companyName, companyPhone));
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println("Error occurred!");
+            System.out.println(ex.getMessage());
+        }
+        return allShippers;
+    }
+
+    public void deleteShipper(int shipperID) {
+        try {
+            String query = """
+                    DELETE FROM shippers
+                    WHERE ShipperID = ?
+                    """;
+
+            try (Connection connection = dataSource.getConnection();
+
+                 PreparedStatement preparedStatement = connection.prepareStatement(query)
+            ) {
+                preparedStatement.setInt(1, shipperID);
+
+                preparedStatement.executeUpdate();
+            }
+        } catch (Exception ex) {
+            System.out.println("Error occurred!");
+            System.out.println(ex.getMessage());
+        }
+    }
 }
--- src/main/java/com/pluralsight/models/Shipper.java
+++ src/main/java/com/pluralsight/models/Shipper.java
@@ -1,4 +1,30 @@
 package com.pluralsight.models;
 
 public class Shipper {
+    private int shipperID;
+    private String companyName;
+    private String phoneNumber;
+
+    public Shipper(int shipperID, String companyName, String phoneNumber) {
+        this.shipperID = shipperID;
+        this.companyName = companyName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getShipperID() {
+        return shipperID;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-9d %-40s %-24s", shipperID, companyName, phoneNumber);
+    }
 }
